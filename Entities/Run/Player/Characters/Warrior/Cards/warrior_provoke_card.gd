5d3222c4f219557c0e2af0283ccaea61d79419f9
Raax <officialraax@gmail.com>
2026-05-06 10:12:20 +0200

    Implemented Warrior Cards sprite frames
    + Defend
    + Earth Shock
    + Fury
    + Intimidate
    + Provoke
    + Rising Dragon

--- Entities/Run/Player/Characters/Warrior/Cards/warrior_provoke_card.gd
+++ Entities/Run/Player/Characters/Warrior/Cards/warrior_provoke_card.gd
@@ -5,22 +5,23 @@ const COUNTER = preload("uid://djoxk0rw6eoc1")
 @export var counter_card: Card
 
 
-func apply_effects(targets: Array[Node], modifiers: ModifierHandler, _from: Node = null) -> void:
+func apply_effects(targets: Array[Node], modifiers: ModifierHandler, _source: Node = null) -> void:
 	var from := modifiers.get_parent()
-	var block_effect := BlockEffect.new()
-	var modified_block = modifiers.get_modified_value(amount, Modifier.Type.BLOCK_GAINED)
-	block_effect.amount = modified_block
-	block_effect.sound = sound
 	
 	if from is Player and sprite_frames:
-		from.play_animation(targets, sprite_frames, target)
-	
-	block_effect.execute([from])
+		from.play_animation(targets, sprite_frames, Card.Target.SELF)
 	
 	for each in targets:
+		var block_effect := BlockEffect.new()
+		var modified_block = modifiers.get_modified_value(amount, Modifier.Type.BLOCK_GAINED)
+		block_effect.amount = modified_block
+		block_effect.sound = sound
+		block_effect.execute([from])
+		
 		var status_effect := StatusEffect.new()
 		var counter := COUNTER.duplicate()
 		counter.card = counter_card
+		counter.stacks += 1
 		status_effect.status = counter
 		status_effect.execute([from])
 
